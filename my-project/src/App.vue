c2d30a892948f92c1bf321d80cd5107d228ebc5f
Noehkc <noetfd@gmail.com>
2019-03-11 16:43:02 -0500

    vue blog commit

---
+++ my-project/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1> {{ title }} </h1>
+        <Navbar />
+        <Blogs />      
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import Blogs from './Blogs'
+
+export default {
+  name: 'app',
+          components: {
+            Navbar,
+            Blogs
+        },
+  data () {
+    return {
+      title: "VUEJS Blog",  
+    }
+  }
+}
+</script>
+
+<style>
+
+body{
+  background-color:#5e646d;
+  font-family:sans-serif;
+}
+h1{
+  color:#444;
+  font-weight:normal;
+  text-align:center;
+  text-transform:uppercase;
+  color:white;
+}
+
+.container{
+  margin:0 auto;
+  width:60%;
+  text-align:center;
+  color:white;
+}
+
+</style>
---
+++ my-project/src/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav>
+        <ul>
+            <li><a href="">Link</a></li>
+            <li><a href="">Link</a></li>
+            <li><a href="">Link</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+
+<style>
+    nav{
+        text-align:center;
+    }
+
+    nav ul{
+        padding:0;
+    }
+
+    nav li{
+        display: inline-block;
+        list-style-type:none;
+        margin:0;
+    }
+
+    nav ul li a{
+        color:aquamarine;
+        text-decoration:none;
+    }
+</style>
